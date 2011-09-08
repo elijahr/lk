7de2a2fae9af05e7bfd0ffeb1dc45a71290e47e5
Elijah Rutschman <elijahr@gmail.com>
2011-09-08 16:09:30 -0700

    okay, you can install with pip now

--- setup.py
+++ setup.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python
+from setuptools import setup
+import sys
 
-import sys, os
-try:
-    from setuptools import setup
-    kw = {'entry_points':
-          """[console_scripts]\nlk = lk:main\n""",
-          'zip_safe': False}
-except ImportError:
-    from distutils.core import setup
-    if sys.platform == 'win32':
-        print('Note: without Setuptools installed you will have to use "python -m lk ENV"')
-        kw = {}
-    else:
-        kw = {'scripts': ['scripts/lk']}
-
-setup(name='lk',
-      version='1.1',
-      description='A programmer\'s search tool, parallel and fast',
-      author='Elijah Rutschman',
-      author_email='elijahr@gmail.com',
-      license='MIT',
-      py_modules=['lk'],
-      keywords='search tool utility grep',
-      classifiers=[
+meta = dict(name='lk',
+    version='1.1',
+    description='A programmer\'s search tool, parallel and fast',
+    author='Elijah Rutschman',
+    author_email='elijahr@gmail.com',
+    license='MIT',
+    install_requires=["setuptools"],
+    py_modules=['lk'],
+    keywords='search tool utility grep',
+    classifiers=[
         'License :: OSI Approved :: MIT License',
         'Topic :: System :: Systems Administration',
         'Topic :: Utilities'
-      ],
-      url='http://github.com/elijahr/lk',
-      **kw)
+    ],
+    url='http://github.com/elijahr/lk',
+    scripts = [
+        'scripts/lk'
+    ],
+)
+
+# Automatic conversion for Python 3 requires distribute.
+if False and sys.version_info >= (3,):
+    meta.update(dict(
+        use_2to3=True,
+    ))
+
+setup(**meta)
